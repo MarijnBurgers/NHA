8d57025884bd53da08b5dafd8481577606b4c0d9
MarijnBurgers <marijnburgers@pm.me>
2023-11-17 22:15:52 +0100

    Update script.js

--- Les25/script/script.js
+++ Les25/script/script.js
@@ -11,10 +11,7 @@ $(document).ready(function(){
    $(document).keydown(function(e){                                 //keydown gebruikt omdat keypressed geen pijltjestoetsen ondersteund (testbrowser: firefox)
         arrowKeyPressed(e);
    });
-   $(document).swiperight(function()
-   {
-        window.alert("swiped right");
-   });
+  
    $('#btRestart').click(function()
    {
         start();
